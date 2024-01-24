ee27da45dc6728660f2de04f1b6d9bcb4bc3a552
sh0stik <dariyashostak@gmail.com>
2024-01-24 11:26:47 -0800

    Added getAlderaanResidents function

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 const residentsBtn = document.querySelector("#residentsButton");
 
-const clickResidentsButton = () => {
-    console.log()
+const getAlderaanResidents = (evt) => {
+    evt.preventDefault();
+    console.log("Button clicked")
+    axios.get("https://swapi.dev/api/planets/", { params: { search: "Alderaan" } })
+        .then(response => {
+            const residents = response.data.results[0].residents;
+            for(const resident of residents){
+                axios.get(resident).then(response => {
+                    const h2 = document.createElement('h2')
+                    h2.textContent = response.data.name
+                    document.body.appendChild(h2)
+                })
+            }
+        })
 }
+residentsBtn.addEventListener("click", getAlderaanResidents);
